4c5ba0969c87e8788a0ab3fed953ee34a895a33f
yamdah <150097822+yamdah@users.noreply.github.com>
2025-05-31 23:32:28 +0900

    Update comm2.sh

--- compare/comm2.sh
+++ compare/comm2.sh
@@ -3,5 +3,43 @@
 # 1
 touch test.txt
 
-echo -n レコード数：
+# 差分を更に比較にかける
+list='wc -l < test.txt'
+echo -n レコード件数：
+echo -n $list
+echo 件
+
+# INSERT
+sort -o test.txt test.txt
+count=0
+echo -en 'INSERT INTO test_table (test_data, created) VALUES'
+while read testData;
+do
+  count='expr $count + 1'
+  echo -en '("'
+  echo -en $testData
+  if [$count -eq $list]; then
+    echo -en '", CURRENT_TIMESTAMP())'
+  else
+    echo -e '", CURRENT_TIMESTAMP()),'
+  fi
+done < test.txt
+echo -e ';'
+
+# DELETE
+sort -o test.txt test.txt
+count=0
+echo -en 'DELETE FROM test_table WHERE test_data IN ('
+while read testData;
+do
+  count='expr $count + 1'
+  echo -en '"'
+  echo -en $testData
+  if [$count -eq $list]; then
+    echo -en '"'
+  else
+    echo -e '",'
+  fi
+done < test.txt
+echo -e ');'
 
